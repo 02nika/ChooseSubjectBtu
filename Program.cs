f8d21adeb8b2b4242e570e5235b27e5bf131236d
nika tsitsilashvili <56062872+02nika@users.noreply.github.com>
2021-09-14 15:07:16 +0400

    program version 1.0

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] subjectIds = new int[] { 4017, 3920 };
+
+            foreach (var subjectId in subjectIds)
+            {
+                GamoigeBtu($"https://classroom.btu.edu.ge/ge/student/me/choose/{subjectId}");
+            }
+        }
+
+        static void GamoigeBtu(string url)
+        {
+            try
+            {
+                Process.Start(url);
+            }
+            catch
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    url = url.Replace("&", "^&");
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", url);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", url);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
